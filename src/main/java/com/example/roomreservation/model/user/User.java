73f483dcb5ec6245aac89013c6cb681b2029151a
nadianMelhem <nadian.melhem.ext@nokia.com>
2023-05-08 17:05:53 +0300

    some fixes

--- src/main/java/com/example/roomreservation/model/user/User.java
+++ src/main/java/com/example/roomreservation/model/user/User.java
@@ -1,5 +1,7 @@
 package com.example.roomreservation.model.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,6 +33,7 @@ public class  User {
 
     @NotBlank
     @Size(max = 50)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
 
